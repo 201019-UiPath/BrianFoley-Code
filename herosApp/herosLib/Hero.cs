64664ee381b489cfd80de1e29409407a8752a9ed
Brian Foley <brianfoley@iMac.local>
2020-10-20 13:32:12 -0600

    herosApp modified

---
+++ herosApp/herosLib/Hero.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace herosLib
+{
+    // default access modifier: internal
+    public class Hero
+    {
+        
+        // default access modifiers: private
+        private int id; // value type => System.Int32
+
+        public int Id {
+            get {
+                return id;
+            }
+            set {
+                id=value;
+            }
+        }
+        public string name; // reference type
+
+        //constructor
+        public Hero(int id, string name) {
+            this.id = id;
+            this.name = name;
+        }
+
+    }
+}
---
+++ herosApp/herosUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using herosLib;
+
+namespace herosUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region default constructor
+            #endregion
+            #region Parameterized constructor
+            Hero obj = new Hero(1, "brian");
+            Console.WriteLine($"{obj.Id} {obj.name}");
+            #endregion 
+            #region access via properties
+            obj.Id = 2;
+            Console.WriteLine($"New Id = {obj.Id}");
+            #endregion
+        }
+    }
+}
